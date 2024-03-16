fc5f87ba29baeda83bea7a2f95b0a8b099eccf4e
anandhhb <anandhh@outlook.in>
2024-03-16 12:57:42 +0530

    Change hardhat.config for sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
+console.log(process.env)
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks:{
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    }
+  }
 };
